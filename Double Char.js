b491d4a0dc3963b935302907c323fcc7c06911e3
Argus-L <91214444+Argus-L@users.noreply.github.com>
2023-03-20 20:19:00 -0400

    Added my solution to the Double CHar problem from CodeWars. 8kyu. Could have used .map() method instead.

---
+++ Double Char.js
@@ -0,0 +1,17 @@
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+Examples (Input -> Output):
+
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+*/
+
+function doubleChar(str) {
+  let arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] + arr[i];
+  }
+  return arr.join('');
+}
